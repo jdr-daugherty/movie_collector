0ef254989e9c93b3a51653eb322df5b883b6741f
jdr-daugherty <john.rothe@daugherty.com>
2022-04-01 11:28:13 -0400

    added logging to controller excepting handling

--- src/main/java/com/daugherty/movie/collector/controller/CollectionControllerAdvice.java
+++ src/main/java/com/daugherty/movie/collector/controller/CollectionControllerAdvice.java
@@ -1,24 +1,38 @@
 package com.daugherty.movie.collector.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
 
+@Slf4j
 @ControllerAdvice
 public class CollectionControllerAdvice {
 
     @ResponseBody
     @ExceptionHandler(MovieNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    String movieNotFoundHandler(MovieNotFoundException ex) {
+    String movieNotFoundHandler(MovieNotFoundException ex, WebRequest request) {
+        log(ex, request);
         return ex.getMessage();
     }
+
     @ResponseBody
     @ExceptionHandler(ReviewNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    String reviewNotFoundHandler(ReviewNotFoundException ex) {
+    String reviewNotFoundHandler(ReviewNotFoundException ex, WebRequest request) {
+        log(ex, request);
         return ex.getMessage();
+    }
+
+    private void log(Exception ex, WebRequest request) {
+        String message = ex.getLocalizedMessage();
+        if( message == null ) {
+            message = ex.toString();
+        }
+        log.error(String.format("%s (%s)", message, request.getDescription(true)));
     }
 }
